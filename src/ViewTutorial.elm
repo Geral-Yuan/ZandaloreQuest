40f71a0059998ff8dc211657ba73be2fac857117
ekkanat_tanchavalit <ekkanat_tanchavalit@sjtu.edu.cn>
2022-07-29 22:50:34 +0800

    perf(viewtutorial.elm): add tutorial texts background

--- src/ViewTutorial.elm
+++ src/ViewTutorial.elm
@@ -1,12 +1,13 @@
 module ViewTutorial exposing (viewTutorialScene)
 
 import Data exposing (..)
+import Debug exposing (toString)
 import Html exposing (Html, button, div)
 import Html.Attributes as HtmlAttr
 import Html.Events exposing (onClick)
 import Message exposing (Msg(..))
 import Model exposing (Model)
-import Svg exposing (Svg, text)
+import Svg exposing (Svg, text, view)
 import Svg.Attributes as SvgAttr
 import ViewOthers exposing (dialogHelper, shapeHelper)
 
@@ -76,12 +77,29 @@ viewHints =
             [ shapeHelper ( 150, 430 ) ( 230, 90 ) "blue" ( 0, 0 )
             , shapeHelper ( 300, 430 ) ( 1780, 165 ) "blue" ( 0, 0 )
             , shapeHelper ( 100, 100 ) (findFixedPos ( 5, 5 )) "blue" ( 0, 0 )
+            , viewHintBackground 600 20 20 170
+            , viewHintBackground 400 20 1250 50
+            , viewHintBackground 600 20 950 700
+            , viewHintBackground 800 20 20 900
             ]
-        , dialogHelper 600 20 20 170 50 "blue" "1. Left click on blue hexagons to move"
-        , dialogHelper 400 20 1250 50 50 "blue" "2. Right click on enemies to attack"
-        , dialogHelper 600 20 950 700 50 "blue" "3. Click T to access all levels and heroes"
-        , dialogHelper 800 20 20 900 50 "blue" "4. Plan a few steps ahead"
+        , dialogHelper 600 20 20 170 50 "white" "1. Left click on blue hexagons to move"
+        , dialogHelper 400 20 1250 50 50 "white" "2. Right click on enemies to attack"
+        , dialogHelper 600 20 950 700 50 "white" "3. Click T to access all levels and heroes"
+        , dialogHelper 800 20 20 900 50 "white" "4. Plan a few steps ahead"
         ]
+
+
+viewHintBackground : Int -> Int -> Int -> Int -> Svg msg
+viewHintBackground w h x y =
+    Svg.rect
+        [ SvgAttr.width (toString w)
+        , SvgAttr.height (toString h)
+        , SvgAttr.x (toString x)
+        , SvgAttr.y (toString y)
+        , SvgAttr.fill "black"
+        , SvgAttr.fillOpacity "70%"
+        ]
+        []
 
 
 viewHintsButton : Html Msg
@@ -123,11 +141,15 @@ viewTutorial1 =
             [ shapeHelper ( 150, 430 ) ( 230, 90 ) "blue" ( 0, 0 )
             , shapeHelper ( 300, 430 ) ( 1780, 165 ) "blue" ( 0, 0 )
             , shapeHelper ( 100, 100 ) (findFixedPos ( 5, 5 )) "blue" ( 0, 0 )
+            , viewHintBackground 600 70 20 170
+            , viewHintBackground 400 130 1250 50
+            , viewHintBackground 600 190 950 700
+            , viewHintBackground 800 70 20 900
             ]
-        , dialogHelper 600 20 20 170 50 "blue" "Enemies Information (Red)"
-        , dialogHelper 400 20 1250 50 50 "blue" "Your heroes' Information (Blue)"
-        , dialogHelper 600 20 950 700 50 "blue" "Brown obstacle: mystery box that drops gold and potions (black means unbreakable)"
-        , dialogHelper 800 20 20 900 50 "blue" "Click anywhere to continue"
+        , dialogHelper 600 20 20 170 50 "white" "Enemies Information (Red)"
+        , dialogHelper 400 20 1250 50 50 "white" "Your heroes' Information (Blue)"
+        , dialogHelper 600 20 950 700 50 "white" "Brown obstacle: mystery box that drops gold and potions (black means unbreakable)"
+        , dialogHelper 800 20 20 900 50 "white" "Click anywhere to continue"
         ]
 
 
@@ -146,8 +168,10 @@ viewTutorial2 =
             [ SvgAttr.width "100%"
             , SvgAttr.height "100%"
             ]
-            [ shapeHelper ( 100, 100 ) (findFixedPos ( 2, 8 )) "blue" ( 0, 0 ) ]
-        , dialogHelper 500 20 500 50 50 "blue" "Left click on Warrior to control it"
+            [ shapeHelper ( 100, 100 ) (findFixedPos ( 2, 8 )) "blue" ( 0, 0 )
+            , viewHintBackground 500 130 500 50
+            ]
+        , dialogHelper 500 20 500 50 50 "white" "Left click on Warrior to control it"
         ]
 
 
@@ -165,8 +189,9 @@ viewTutorial3 =
             , SvgAttr.height "100%"
             ]
             [ shapeHelper ( 100, 100 ) (findFixedPos ( 3, 7 )) "blue" ( 3, 7 )
+            , viewHintBackground 700 250 500 50
             ]
-        , dialogHelper 700 30 500 50 50 "blue" "Blue hexagons denote the attackable range of the hero. Left click the adjacent hexagon to move."
+        , dialogHelper 700 30 500 50 50 "white" "Blue hexagons denote the attackable range of the hero. Left click the adjacent hexagon to move."
         ]
 
 
@@ -184,8 +209,9 @@ viewTutorial4 =
             , SvgAttr.height "100%"
             ]
             [ shapeHelper ( 100, 100 ) (findFixedPos ( 4, 6 )) "blue" ( 4, 6 )
+            , viewHintBackground 800 190 580 50
             ]
-        , dialogHelper 800 30 580 50 50 "blue" "Each hero has an energy limit. -2 to move and -3 to attack. Energy refreshes after each turn. Left click to move."
+        , dialogHelper 800 30 580 50 50 "white" "Each hero has an energy limit. -2 to move and -3 to attack. Energy refreshes after each turn. Left click to move."
         ]
 
 
@@ -203,8 +229,9 @@ viewTutorial5 =
             , SvgAttr.height "100%"
             ]
             [ shapeHelper ( 100, 100 ) (findFixedPos ( 2, 7 )) "blue" ( 0, 0 )
+            , viewHintBackground 400 130 580 50
             ]
-        , dialogHelper 400 20 580 50 50 "blue" "Now left click on archer."
+        , dialogHelper 400 20 580 50 50 "white" "Now left click on archer."
         ]
 
 
@@ -222,8 +249,9 @@ viewTutorial6 =
             , SvgAttr.height "100%"
             ]
             [ shapeHelper ( 100, 100 ) (findFixedPos ( 5, 4 )) "blue" ( 5, 4 )
+            , viewHintBackground 700 190 580 50
             ]
-        , dialogHelper 700 20 580 50 50 "blue" "Right click on the crate to attack. A random item (health/energy potion or gold) will be dropped."
+        , dialogHelper 700 20 580 50 50 "white" "Right click on the crate to attack. A random item (health/energy potion or gold) will be dropped."
         ]
 
 
@@ -251,8 +279,9 @@ viewTutorial7 =
                 , SvgAttr.fill "transparent"
                 ]
                 []
+            , viewHintBackground 700 190 850 800
             ]
-        , dialogHelper 700 20 850 800 50 "blue" "Click the end turn button to end your turn wait till the enemy turn is over."
+        , dialogHelper 700 20 850 800 50 "white" "Click the end turn button to end your turn wait till the enemy turn is over."
         ]
 
 
@@ -270,8 +299,9 @@ viewTutorial8 =
             , SvgAttr.height "100%"
             ]
             [ shapeHelper ( 100, 100 ) (findFixedPos ( 4, 6 )) "blue" ( 4, 6 )
+            , viewHintBackground 700 130 580 50
             ]
-        , dialogHelper 700 20 580 50 50 "blue" "Now it is your turn, left click on the warrior"
+        , dialogHelper 700 20 580 50 50 "white" "Now it is your turn, left click on the warrior"
         ]
 
 
@@ -289,8 +319,9 @@ viewTutorial9 =
             , SvgAttr.height "100%"
             ]
             [ shapeHelper ( 100, 100 ) (findFixedPos ( 4, 5 )) "blue" ( 4, 5 )
+            , viewHintBackground 700 70 580 50
             ]
-        , dialogHelper 700 20 580 50 50 "blue" "Now move it here"
+        , dialogHelper 700 20 580 50 50 "white" "Now move it here"
         ]
 
 
@@ -308,8 +339,9 @@ viewTutorial10 =
             , SvgAttr.height "100%"
             ]
             [ shapeHelper ( 100, 100 ) (findFixedPos ( 5, 4 )) "blue" ( 5, 4 )
+            , viewHintBackground 700 190 580 50
             ]
-        , dialogHelper 700 20 580 50 50 "blue" "Move the warrior onto the item and it will receive +10 health/+2 energy."
+        , dialogHelper 700 20 580 50 50 "white" "Move the warrior onto the item and it will receive +10 health/+2 energy."
         ]
 
 
@@ -327,8 +359,9 @@ viewTutorial11 =
             , SvgAttr.height "100%"
             ]
             [ shapeHelper ( 100, 100 ) (findFixedPos ( 2, 7 )) "blue" ( 2, 7 )
+            , viewHintBackground 400 130 580 50
             ]
-        , dialogHelper 400 20 580 50 50 "blue" "Now left click on archer."
+        , dialogHelper 400 20 580 50 50 "white" "Now left click on archer."
         ]
 
 
@@ -346,8 +379,9 @@ viewTutorial12 =
             , SvgAttr.height "100%"
             ]
             [ shapeHelper ( 100, 100 ) (findFixedPos ( 7, 2 )) "blue" ( 7, 2 )
+            , viewHintBackground 600 190 580 50
             ]
-        , dialogHelper 600 20 580 50 50 "blue" "Now right click on the enemy hero to attack it. Crticial damage may be dealt."
+        , dialogHelper 600 20 580 50 50 "white" "Now right click on the enemy hero to attack it. Crticial damage may be dealt."
         ]
 
 
@@ -364,6 +398,6 @@ viewTutorial13 =
             [ SvgAttr.width "100%"
             , SvgAttr.height "100%"
             ]
-            []
-        , dialogHelper 850 30 580 50 50 "blue" "I have been guiding you and now it is time for you to destroy the enemy by yourself. Good luck hero! Click anywhere to continue."
+            [ viewHintBackground 850 250 580 50 ]
+        , dialogHelper 850 30 580 50 50 "white" "I have been guiding you and now it is time for you to destroy the enemy by yourself. Good luck hero! Click anywhere to continue."
         ]
