145325c259d84a0726ca714c833d0b9c7556c786
Yuan Jiale <yjl2030906598@sjtu.edu.cn>
2022-08-02 00:41:00 +0800

    style(listoperation.elm): add annotation for list function in ListOperation.elm file

--- src/ListOperation.elm
+++ src/ListOperation.elm
@@ -1,9 +1,15 @@
 module ListOperation exposing (..)
 
+{- get the intersection of two lists -}
+
 
 listIntersection : List a -> List a -> List a
 listIntersection list1 list2 =
     List.filter (\x -> List.member x list2) list1
+
+
+
+{- get the intersection of all lists which are elements of input list -}
 
 
 intersectionList : List (List a) -> List a
@@ -19,9 +25,17 @@ intersectionList llist =
             intersectionList (listIntersection list1 list2 :: rest)
 
 
+
+{- get list of elements in list1 but not in list2 -}
+
+
 listDifference : List a -> List a -> List a
 listDifference list1 list2 =
     List.filter (\x -> not (List.member x list2)) list1
+
+
+
+{- get the union of two lists -}
 
 
 listUnion : List a -> List a -> List a
@@ -31,6 +45,10 @@ listUnion list1 list2 =
             List.filter (\x -> not (List.member x list2)) list1
     in
     list2 ++ newElements
+
+
+
+{- get the union of all lists which are elements of input list -}
 
 
 unionList : List (List a) -> List a
